57b7297a967a7056866b301b9997752d30801963
Aaron Renner <aaron.renner@gmail.com>
2025-06-07 04:20:54 +0000

    WIP - generate config module functions

--- test/mix/tasks/sims.gen.basic_http_test.exs
+++ test/mix/tasks/sims.gen.basic_http_test.exs
@@ -8,6 +8,51 @@ defmodule Mix.Tasks.Sims.Gen.BasicHttpTest do
       test_project(app_name: :my_app)
       |> Igniter.compose_task("sims.gen.basic_http", ~w(PaymentGateway))
       |> assert_creates("test/support/payment_gateway_simulator.ex")
+      |> assert_creates("lib/my_app/config.ex", """
+      defmodule MyApp.Config do
+        @moduledoc \"""
+        Main configuration module for the application
+        \"""
+
+        alias MyApp.Config.DefaultAdapter
+
+        @behaviour MyApp.Config.Adapter
+
+        @doc \"""
+        Base url for the Payment Gateway server
+        \"""
+        @impl true
+        def payment_gateway_base_url do
+          adapter().payment_gateway_base_url()
+        end
+
+        defp adapter do
+          Application.get_env(:my_app, :config_adapter, DefaultAdapter)
+        end
+      end
+      """)
+      |> assert_creates("lib/my_app/config/adapter.ex", """
+      defmodule MyApp.Config.Adapter do
+        @moduledoc false
+        @callback payment_gateway_base_url() :: String.t()
+      end
+      """)
+      |> assert_creates("lib/my_app/config/default_adapter.ex", """
+      defmodule MyApp.Config.DefaultAdapter do
+        @moduledoc false
+
+        @behaviour MyApp.Config.Adapter
+        @impl true
+        def payment_gateway_base_url do
+          # TODO Configure this in config/config.exs or config/runtime.exs
+          Application.fetch_env!(:my_app, :payment_gateway_base_url)
+        end
+      end
+      """)
+      |> assert_has_patch("test/test_helper.exs", """
+      + |Mox.defmock(MyApp.Config.MockAdapter, for: MyApp.Config.Adapter)
+      + |Application.put_env(:my_app, :config_adapter, MyApp.Config.MockAdapter)
+      """)
 
     diff = diff(igniter)
 
