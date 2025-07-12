333a97d2298996a7fce92138bf033b16219b5dca
Aaron Renner <aaron@renner.live>
2025-07-12 16:09:34 +0000

    Add --include-response-stubs opt to HTTPCrud

--- integration_test/test/sims/http_crud_test.exs
+++ integration_test/test/sims/http_crud_test.exs
@@ -28,4 +28,21 @@ defmodule Sims.Integration.HttpCrudTest do
     assert "test/support/blog_simulator.ex" in paths
     assert "test/sample_app/blog_simulator_test.exs" in paths
   end
+
+  @tag :tmp_dir
+  test "with response stubs", %{tmp_dir: tmp_dir} do
+    app_path = generate_project(tmp_dir)
+
+    mix_run!(
+      ~w(sims.gen.http_crud Blog post posts --include-tests --include-response-stubs --yes),
+      app_path
+    )
+
+    mix_run!(~w(test), app_path)
+
+    paths = list_project_files(app_path)
+
+    assert "test/support/blog_simulator.ex" in paths
+    assert "test/sample_app/blog_simulator_test.exs" in paths
+  end
 end
