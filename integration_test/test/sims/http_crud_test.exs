826ab087edba2881367305842145e788d042bf13
Aaron Renner <aaron@renner.live>
2025-07-13 14:00:34 +0000

    Add --include-app-config to http_crud

--- integration_test/test/sims/http_crud_test.exs
+++ integration_test/test/sims/http_crud_test.exs
@@ -8,12 +8,23 @@ defmodule Sims.Integration.HttpCrudTest do
     app_path = generate_project(tmp_dir)
 
     mix_run!(~w(sims.gen.http_crud AddressBook contact contacts --include-tests --yes), app_path)
+    mix_run!(~w(sims.gen.http_crud Library book books --include-tests --yes), app_path)
     mix_run!(~w(test), app_path)
 
     paths = list_project_files(app_path)
 
     assert "test/support/address_book_simulator.ex" in paths
     assert "test/sample_app/address_book_simulator_test.exs" in paths
+    assert "test/support/library_simulator.ex" in paths
+    assert "test/sample_app/library_simulator_test.exs" in paths
+
+    assert app_path
+           |> Path.join("test/test_helper.exs")
+           |> File.read!() == """
+           ExUnit.start()
+           Mox.defmock(SampleApp.Config.MockAdapter, for: SampleApp.Config.Adapter)
+           Application.put_env(:sample_app, :config_adapter, SampleApp.Config.MockAdapter)
+           """
   end
 
   @tag :tmp_dir
@@ -44,5 +55,17 @@ defmodule Sims.Integration.HttpCrudTest do
 
     assert "test/support/blog_simulator.ex" in paths
     assert "test/sample_app/blog_simulator_test.exs" in paths
+  end
+
+  @tag :tmp_dir
+  test "with no app config", %{tmp_dir: tmp_dir} do
+    app_path = generate_project(tmp_dir)
+
+    mix_run!(
+      ~w(sims.gen.http_crud Blog post posts --include-tests --no-include-app-config --yes),
+      app_path
+    )
+
+    mix_run!(~w(test), app_path)
   end
 end
--- test/mix/tasks/sims.gen.http_crud_test.exs
+++ test/mix/tasks/sims.gen.http_crud_test.exs
@@ -8,6 +8,73 @@ defmodule Mix.Tasks.Sims.Gen.HttpCrudTest do
       test_project(app_name: :my_app)
       |> Igniter.compose_task("sims.gen.http_crud", ~w(AddressBook contact contacts))
       |> assert_creates("test/support/address_book_simulator.ex")
+      |> assert_creates("test/support/simulator_helpers.ex")
+      |> assert_creates("lib/my_app/config.ex", """
+      defmodule MyApp.Config do
+        @moduledoc \"""
+        Main configuration module for the application
+        \"""
+
+        alias MyApp.Config.DefaultAdapter
+
+        @behaviour MyApp.Config.Adapter
+
+        @doc \"""
+        Base url for the Address Book server
+        \"""
+        @impl true
+        def address_book_base_url do
+          adapter().address_book_base_url()
+        end
+
+        defp adapter do
+          Application.get_env(:my_app, :config_adapter, DefaultAdapter)
+        end
+      end
+      """)
+      |> assert_creates("lib/my_app/config/adapter.ex", """
+      defmodule MyApp.Config.Adapter do
+        @moduledoc false
+        @callback address_book_base_url() :: String.t()
+      end
+      """)
+      |> assert_creates("lib/my_app/config/default_adapter.ex", """
+      defmodule MyApp.Config.DefaultAdapter do
+        @moduledoc false
+
+        @behaviour MyApp.Config.Adapter
+        @impl true
+        def address_book_base_url do
+          # TODO Configure this in config/config.exs or config/runtime.exs
+          Application.fetch_env!(:my_app, :address_book_base_url)
+        end
+      end
+      """)
+      |> assert_creates("test/support/simulator_helpers.ex", """
+      defmodule MyApp.SimulatorHelpers do
+        @moduledoc \"""
+        Helper functions for configuring the application to work with test simulators.
+        \"""
+
+        import ExUnit.Callbacks
+
+        @doc \"""
+        Configure the application to use the Address Book simulator.
+        \"""
+        def configure_for_address_book_simulator(_tags) do
+          sim = start_supervised!(MyApp.AddressBookSimulator)
+
+          base_url = MyApp.AddressBookSimulator.base_url(sim)
+          Mox.stub(MyApp.Config.MockAdapter, :address_book_base_url, fn -> base_url end)
+
+          [address_book_simulator: sim]
+        end
+      end
+      """)
+      |> assert_has_patch("test/test_helper.exs", """
+      + |Mox.defmock(MyApp.Config.MockAdapter, for: MyApp.Config.Adapter)
+      + |Application.put_env(:my_app, :config_adapter, MyApp.Config.MockAdapter)
+      """)
 
     diff = diff(igniter)
 
@@ -46,6 +113,17 @@ defmodule Mix.Tasks.Sims.Gen.HttpCrudTest do
     diff = diff(igniter)
 
     assert diff =~ "def trigger_internal_server_errors"
+  end
+
+  test "does not generate config with --no-include-app-config" do
+    test_project(app_name: :my_app)
+    |> Igniter.compose_task(
+      "sims.gen.http_crud",
+      ~w(AddressBook contact contacts --no-include-app-config)
+    )
+    |> refute_creates("lib/my_app/config.ex")
+    |> refute_creates("lib/my_app/config/adapter.ex")
+    |> refute_creates("lib/my_app/config/default_adapter.ex")
   end
 
   test "errors when passing a simulator name with invalid characters" do
